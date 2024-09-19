eb92088376f7f7f01893bfd2227d61ae8d314c0a
Giovanni Benedet Rodrigues <gi-benedet@hotmail.com>
2024-09-19 12:25:41 -0300

    Edição de codigo
    código mais simplificado e de fácil leitura.

--- Script/Script2.py
+++ Script/Script2.py
@@ -3,4 +3,4 @@
 num1 = int(input('Digite um numero:'))
 num2 = int(input('digite outro numero:'))
 
-print('A soma de {} e {}'.format(num2, num1), 'é {}'.format(num1 + num2))
+print('A soma de {} e {} é {}'.format(num2, num1, num2 + num1))
