e7781d7981b82733e7c003d0ef85087d14c93f87
Giovanni Benedet Rodrigues <gi-benedet@hotmail.com>
2024-09-19 12:16:46 -0300

    EX 1 parte 1
    faça o usúario digitar dois números e some eles.

---
+++ Script/Script.py
@@ -0,0 +1,6 @@
+#Faça o usuario digitar dois numeros e some eles
+
+num1 = int(input('Digite um numero:'))
+num2 = int(input('Digite outro numero:'))
+
+print('o resultado é: {}'.format(num1 + num2))
