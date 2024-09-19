b5e3fd4896562cfd89dfe25324daa64de1cea91f
Giovanni Benedet Rodrigues <gi-benedet@hotmail.com>
2024-09-19 12:22:47 -0300

    EX 1 parte 2
    Faça o usuario digitar dois numeros e mostre os numeros somados e o resultado deles

---
+++ Script/Script2.py
@@ -0,0 +1,6 @@
+#Faça o usuario digitar dois numeros e mostre os numeros somados e o resultado deles
+
+num1 = int(input('Digite um numero:'))
+num2 = int(input('digite outro numero:'))
+
+print('A soma de {} e {}'.format(num2, num1), 'é {}'.format(num1 + num2))
